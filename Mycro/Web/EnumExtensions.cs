8be8a73db7de73bfca11b1041549d6810a740d77
TELEVIC\PID <p.deparcq@televic.com>
2017-08-31 16:04:36 +0200

    Added EnumExtensions

---
+++ Mycro/Web/EnumExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Televic.Mycro.Web
+{
+    public static class EnumExtensions
+    {
+        public static string GetName<T>(this T value) where T : struct
+        {
+            return Enum.GetName(typeof(T), value).ToLower();
+        }
+
+        public static T Parse<T>(this string value, T defaultValue) where T : struct
+        {
+            T parsed;
+            return Enum.TryParse(value, true, out parsed) ? parsed : defaultValue;
+        }
+    }
+}
